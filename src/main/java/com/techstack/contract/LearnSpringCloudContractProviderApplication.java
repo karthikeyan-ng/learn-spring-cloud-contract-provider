67b0bde1244cba9c8ea557bf8ce1eca63f190a1a
Karthikeyan Nithiyanandam <rabodevops17@RABODEVOPS17s-MacBook-Pro.local>
2020-03-20 21:27:43 +0530

    Initial commit

---
+++ src/main/java/com/techstack/contract/LearnSpringCloudContractProviderApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudContractProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCloudContractProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techstack/contract/LearnSpringCloudContractProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudContractProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
