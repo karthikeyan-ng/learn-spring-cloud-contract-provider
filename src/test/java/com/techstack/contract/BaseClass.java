d2162e484a828eec87957b8d03c763702fa2e419
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-20 22:56:01 +0530

    close #5: Implemented BaseClass Test

--- src/test/java/com/techstack/contract/BaseClass.java
+++ src/test/java/com/techstack/contract/BaseClass.java
@@ -1,4 +1,18 @@
 package com.techstack.contract;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class BaseClass {
+
+    @Autowired
+    EmployeeController employeeController;
+
+    @BeforeEach
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(employeeController);
+    }
 }
