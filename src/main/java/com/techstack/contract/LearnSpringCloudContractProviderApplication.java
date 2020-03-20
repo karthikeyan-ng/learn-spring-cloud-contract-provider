ba387efb7e5886e1ed604166f41f8207a2575ab3
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-20 22:40:55 +0530

    close #4: Created Rest controller

--- src/main/java/com/techstack/contract/LearnSpringCloudContractProviderApplication.java
+++ src/main/java/com/techstack/contract/LearnSpringCloudContractProviderApplication.java
@@ -2,6 +2,12 @@ package com.techstack.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class LearnSpringCloudContractProviderApplication {
@@ -10,4 +16,13 @@ public class LearnSpringCloudContractProviderApplication {
 		SpringApplication.run(LearnSpringCloudContractProviderApplication.class, args);
 	}
 
+}
+
+@RestController
+class EmployeeController {
+
+	@GetMapping("/employees")
+	public ResponseEntity<List<String>> getEmployees() {
+		return ResponseEntity.status(HttpStatus.OK).body(List.of("pascal", "thomas"));
+	}
 }
